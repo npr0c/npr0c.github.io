f95ae98c854a65c18ef9b9878edf4c77b17c4e6e
npr0c <230718710+npr0c@users.noreply.github.com>
2025-09-07 20:34:26 -0300

    Create s0ngs.js

---
+++ s0ngs.js
@@ -0,0 +1,16 @@
+/* aud functions */
+<audio id="syn">
+</audio>
+  /* handles loopin n stuff */
+  <script>
+    const ack = document.getElementById("syn");
+    const tracks = ["h8.mp3", "wow.mp3"];
+
+    function play() {                  
+    const rand = tracks[Math.floor(Math.random() * tracks.length)];    
+    ack.src = rand;                                                    
+    ack.play();
+  }
+    document.body.addEventListener("click", play);
+    ack.addEventListener("ended", play);
+  </script>
